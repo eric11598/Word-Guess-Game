0c7186bf87eb2ca5a1539658579f1f7b3ecec921
eric11598 <eric.arellano.0803@gmail.com>
2018-06-30 18:07:03 -0400

    final changes

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # Word-Guess-Game
 
-https://nintendoenthusiast.com/blog/2018/06/20/super-smash-bros-ultimate-official-roster-artwork-all-65-characters/
+Super smash brothers themed word guessing game!
+
+Features:
+- Music that auto plays but can be paused/started with a button
+- Silhouette images to guess, turns into image of character when guessed correct / plays sound from game
+- Modular design - Can add more characters through creating an object and adding to character array
+
 
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -108,181 +108,195 @@
           imageSilhouette: "assets/images/nessSil.jpg",
           announcerSound: "assets/sounds/snd_se_narration_characall_Ness.wav",
         }
-     
-    var score = 0;
-    var questionIndex = 0;
-    var playerWins = 0;
-    var playerLosses = 0;
-    var playerGuesses = 0;
-    var pauseGame = false;
 
-    var characterArray = [snake,peach,kirby,mario,fox,ness,luigi,pikachu,samus,yoshi];
-    var character = Math.floor(Math.random() * 10);      // returns a random integer from 0 to 3
+//MUSIC CONTROL
 
-    
-    document.getElementById("characterImage").src= characterArray[character].imageSilhouette;
-    
+var music = new Audio("assets/sounds/theme_extended.mp3");
+var musicPlaying = true;
+music.play();
 
-    var pos = 0;
-    var answerArraySet = ["answer1", "answer2","answer3","answer4","answer5","answer6", "answer7","answer8","answer9","answer10"];
-         
-    var guessArray = ["guess1", "guess2","guess3","guess4","guess5","guess6", "guess7","guess8","guess9","guess10"];
-    var setArray = ["guess1", "guess2","guess3","guess4","guess5","guess6", "guess7","guess8","guess9","guess10"];
-
-
-      var correct = 0;
-
-
-      function checkDuplicate(userInput) {
-
-        var duplicate = 0;
-
-        for (i=0; i<setArray.length;i++)
-        {
-          if(userInput == setArray[i])
-            duplicate++;
-        }
-
-        if(duplicate==2)
-          return true;   
-
-        else
-          return false;
-      
-      }
-
-      function checkWin(temp){
-
-        if(temp == correct)
-        {
-          correct = 0;
-          playerGuesses = 0;
-          return true;
-        }
-      }
-
-
-
-        function reset(){
-
-          playerGuesses = 0;
-          correct = 0;
-
-          guessArray = ["guess1", "guess2","guess3","guess4","guess5","guess6", "guess7","guess8","guess9","guess10"];
-          setArray = ["guess1", "guess2","guess3","guess4","guess5","guess6", "guess7","guess8","guess9","guess10"];
-          pos = 0;
-          character = Math.floor(Math.random() * 10);
-         
-          document.getElementById("characterImage").src= characterArray[character].imageSilhouette;
-          document.getElementById("status").innerHTML = "";
-          var temp = document.getElementById("letters");
-
-          for (i =0 ; i <guessArray.length; i++)
-            document.getElementById(guessArray[i]).innerHTML = "_";
-
-          while (temp.hasChildNodes()) {  
-            temp.removeChild(temp.firstChild);
-      } 
-        
-      
-      for (s=0; s<characterArray[character].length; s++)
-      {
-        g = document.createElement('p');
-        g.setAttribute("id", answerArraySet[s]);
-        g.setAttribute("style", "display: inline;"); 
-        g.innerHTML="_ ";
-        document.getElementById("letters").appendChild(g); 
-
-      }
-
-        }
-
-
-     
-      reset();
-
-      function gameStart(userInput){
-
-      
-        setArray[pos] = userInput;
-
-        if(checkDuplicate(userInput))
-        {
-          console.log("duplicate");
-          alert("duplicate");
-        }
-
-        else
-        {
-          document.getElementById(guessArray[pos]).innerHTML = userInput;
-
-          for(j=0; j<characterArray[character].length;j++)
-          {
-            if(userInput==characterArray[character].nameArray[j])
-            {
-              document.getElementById(answerArraySet[j]).innerHTML = userInput;
-              correct++;
-            }
-          }
-          playerGuesses++;
-          pos++;
-
-
-          if (checkWin(characterArray[character].length))
-            {
-              document.getElementById("status").innerHTML = "YOU WIN ! PRESS ANY KEY TO PLAY AGAIN"
-              playerWins++;
-              pauseGame = true;
-              document.getElementById("characterImage").src= characterArray[character].image;
-              console.log(characterArray[character].announcerSound);
-
-              var audio = new Audio(characterArray[character].announcerSound);
-              audio.play();
-              
-            }
-
-            if(playerGuesses==10)
-            {
-              document.getElementById("status").innerHTML = "YOU LOSE ! PRESS ANY KEY TO PLAY AGAIN"
-              
-              playerLosses++;
-              
-              pauseGame = true;
-            }
-
-      
-        document.getElementById("wins").innerHTML = playerWins;
-        document.getElementById("losses").innerHTML = playerLosses;
-
-      
-        }
+function musicControl(){
+  if(musicPlaying)
+  {
+    music.pause();
+    musicPlaying = false;
+  }
   
-      }
+  else
+  {
+    music.play();
+    musicPlaying = true;
+  }
+}
 
-      var value = 33;
-        function myFunction(){
-          var value = 66;
-        }
+
+document.getElementById("status").innerHTML = "GUESS A LETTER!";
 
 
-      document.onkeyup = function(event) {
+var score = 0;
+var questionIndex = 0;
+var playerWins = 0;
+var playerLosses = 0;
+var playerGuesses = 0;
+var pauseGame = false;
 
-        myFunction();
-        console.log(value);
+var characterArray = [snake,peach,kirby,mario,fox,ness,luigi,pikachu,samus,yoshi];
+var character = Math.floor(Math.random() * 10);      // returns a random integer from 0 to 3
+
+    
+document.getElementById("characterImage").src= characterArray[character].imageSilhouette;
+    
+
+var pos = 0;
+var answerArraySet = ["answer1", "answer2","answer3","answer4","answer5","answer6", "answer7","answer8","answer9","answer10"];
+         
+var guessArray = ["guess1", "guess2","guess3","guess4","guess5","guess6", "guess7","guess8","guess9","guess10"];
+var setArray = ["guess1", "guess2","guess3","guess4","guess5","guess6", "guess7","guess8","guess9","guess10"];
+
+
+var correct = 0;
+
+
+//CHECKS FOR DUPLICATES
+
+function checkDuplicate(userInput) {
+  var duplicate = 0;
+
+  for (i=0; i<setArray.length;i++)
+  {
+    if(userInput == setArray[i])
+      duplicate++;
+  }
+
+  if(duplicate==2)
+      return true;   
+
+  else
+      return false;
+      
+  }
+
+//CHECKS FOR WINS
+
+function checkWin(temp){
+  if(temp == correct)
+  {
+    correct = 0;
+    playerGuesses = 0;
+    return true;
+    }
+}
+
+
+//RESETS ALL VALUES AND SETS NEW CHARACTER
+
+function reset(){
+
+  playerGuesses = 0;
+  correct = 0;
+
+  guessArray = ["guess1", "guess2","guess3","guess4","guess5","guess6", "guess7","guess8","guess9","guess10"];
+  setArray = ["guess1", "guess2","guess3","guess4","guess5","guess6", "guess7","guess8","guess9","guess10"];
+  pos = 0;
+  character = Math.floor(Math.random() * 10);
+         
+  document.getElementById("characterImage").src= characterArray[character].imageSilhouette;
+  document.getElementById("status").innerHTML = "GUESS A LETTER!";
+  var temp = document.getElementById("letters");
+
+  for (i =0 ; i <guessArray.length; i++)
+    document.getElementById(guessArray[i]).innerHTML = "_";
+
+  while (temp.hasChildNodes())  
+    temp.removeChild(temp.firstChild);
+      
         
-        if(pauseGame)
-        {
-          console.log(pauseGame);
-          pauseGame = false;
-          reset();
-        }
-        else
-        {
-          var userInput = event.key.toLowerCase();
-          gameStart(userInput);
-        }
+  for (s=0; s<characterArray[character].length; s++)
+  {
+    g = document.createElement('p');
+    g.setAttribute("id", answerArraySet[s]);
+    g.setAttribute("style", "display: inline;"); 
+    g.innerHTML="_ ";
+    document.getElementById("letters").appendChild(g); 
+  }
 
-      };
+}
+
+
+reset();
+
+//STARTS GAME
+
+function gameStart(userInput){
+  setArray[pos] = userInput;
+  
+  if(checkDuplicate(userInput))
+  {
+    console.log("duplicate");
+    alert("Already guessed that letter!");
+  }
+  
+  else
+  {
+    document.getElementById(guessArray[pos]).innerHTML = userInput;
+
+    for(j=0; j<characterArray[character].length;j++)
+    {
+      if(userInput==characterArray[character].nameArray[j])
+      {
+        document.getElementById(answerArraySet[j]).innerHTML = userInput;
+        correct++;
+      }
+    }
+    
+  playerGuesses++;
+  pos++;
+
+
+  if (checkWin(characterArray[character].length))
+  {
+    document.getElementById("status").innerHTML = "YOU WIN ! PRESS ANY KEY TO PLAY AGAIN"
+    playerWins++;
+    pauseGame = true;
+    document.getElementById("characterImage").src= characterArray[character].image;
+    console.log(characterArray[character].announcerSound);
+
+    var audio = new Audio(characterArray[character].announcerSound);
+    audio.play();
+              
+  }
+
+  if(playerGuesses==10)
+  {
+    document.getElementById("status").innerHTML = "YOU LOSE ! PRESS ANY KEY TO PLAY AGAIN"
+    playerLosses++;
+    pauseGame = true;
+  }
+   
+  document.getElementById("wins").innerHTML = playerWins;
+  document.getElementById("losses").innerHTML = playerLosses;
+ 
+  } 
+}
+
+
+//MAIN 
+
+document.onkeyup = function(event) {
+  
+  if(pauseGame)
+  {
+    console.log(pauseGame);
+    pauseGame = false;
+    reset();
+  }
+  
+  else
+  {
+    var userInput = event.key.toLowerCase();
+    gameStart(userInput);
+  }
+};
 
     
    
